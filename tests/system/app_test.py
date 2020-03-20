8c9e94699aad3a361275242ee0732005acc5881a
remedialchaostheory <remedialchaostheory@users.noreply.github.com>
2020-03-19 22:55:11 -0700

    Add test for print multiple blogs

--- tests/system/app_test.py
+++ tests/system/app_test.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from unittest.mock import patch
+from unittest.mock import patch, call
 import app
 from blog import Blog
 
@@ -12,4 +12,18 @@ class AppTest(TestCase):
             app.print_blogs()
             mocked_print.assert_called_with(
                 '- Test Blog by Test Author (0 posts)'
+            )
+
+    def test_print_multiple_blogs(self):
+        blog = Blog('Test Blog', 'Test Author')
+        blog_2 = Blog('Test Blog 2', 'Test Author 2')
+        app.blogs = {'Test': blog, 'Test 2': blog_2}
+
+        with patch('builtins.print') as mocked_print:
+            app.print_blogs()
+            assert mocked_print.call_args_list[0] == call(
+                '- Test Blog by Test Author (0 posts)'
+            )
+            assert mocked_print.call_args_list[1] == call(
+                '- Test Blog 2 by Test Author 2 (0 posts)'
             )
