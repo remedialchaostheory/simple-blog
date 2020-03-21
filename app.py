d562ba8b209d619811e03dbdf2633ab9325489c1
remedialchaostheory <remedialchaostheory@users.noreply.github.com>
2020-03-20 17:04:35 -0700

    Add test for menu()

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
-blogs = dict()  # blog_name : blog object
+MENU_PROMPT = 'Enter "c" to create a blog, "l" to list blogs, "r" to read one, "p" to create a post, or "q" to quit'
+
+# blogs = dict()  # blog_name : blog object
+from blog import Blog
+
+blog = Blog('Test Blog', 'Test Author')
+blog_2 = Blog('Test Blog 2', 'Test Author 2')
+blogs = {'Test': blog, 'Test 2': blog_2}
 
 # blogs = {
 #     {
@@ -19,6 +26,7 @@ def menu():
     # Eventual wait
 
     print_blogs()
+    selection = input(MENU_PROMPT)
 
 
 def print_blogs():
